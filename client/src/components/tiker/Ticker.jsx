bf9fffc05a003746a8e606956b2f954c477eb535
Ira Akulova <99876200+IraAkulova@users.noreply.github.com>
2024-03-06 10:45:32 +0200

    changed colors

--- client/src/components/tiker/Ticker.jsx
+++ client/src/components/tiker/Ticker.jsx
@@ -31,12 +31,13 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-  const red = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-  const green = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-  const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+  const green = Math.floor(Math.random() * 200 + 55).toString(16).padStart(2, '0');
+  const red = Math.floor(Math.random() * 128).toString(16).padStart(2, '0');
+  const blue = Math.floor(Math.random() * 128).toString(16).padStart(2, '0');
 
   return `#${red}${green}${blue}`;
 }
+
 
 
 export default Ticker;
